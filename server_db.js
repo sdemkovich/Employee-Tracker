a0cb80cf57581a6800ddc2877f1ee46184a80ca7
Sveta Demkovich <svetlana.demkovich@gmail.com>
2020-08-15 12:14:20 -0700

    add Library

--- server_db.js
+++ server_db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.DB_PW,
+    password: process.env,
     database: "employeeTracker_db"
 });
 
